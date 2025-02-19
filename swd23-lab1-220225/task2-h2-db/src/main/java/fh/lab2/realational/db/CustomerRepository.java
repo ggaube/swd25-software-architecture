3254f6e2c3065e77bece7b19a47756a243c1e39e
fh-lecture-labs <78953778+ggaube@users.noreply.github.com>
2025-02-19 13:45:24 +0100

    Add files via upload

---
+++ swd23-lab1-220225/task2-h2-db/src/main/java/fh/lab2/realational/db/CustomerRepository.java
@@ -0,0 +1,11 @@
+package fh.lab2.realational.db;
+
+import java.util.List;
+import org.springframework.data.repository.CrudRepository;
+
+public interface CustomerRepository extends CrudRepository<Customer, Long> {
+
+    List<Customer> findByLastName(String lastName);
+
+    Customer findById(long id);
+}
